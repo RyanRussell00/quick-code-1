45ecc6453fa596a24f540bbf79724884581bf879
Ryan Russell <35180491+RyanRussell00@users.noreply.github.com>
2021-07-03 14:25:56 -0700

    Update POST-DynamoDB-NodeJS.js

--- POST-DynamoDB-NodeJS.js
+++ POST-DynamoDB-NodeJS.js
@@ -1,26 +1,32 @@
 const AWS = require('aws-sdk');  // AWS software development kit
-const client = new AWS.DynamoDB.DocumentClient();  // create new instance? 
+const dynamoDBClient = new AWS.DynamoDB.DocumentClient();  // create new instance? 
 
+/*
+!! NOTE:
+The body of the event must contain the data that you want inserted as a JSON.
+The keys will become the name of the column in DynamoDB, make sure they're correct!!
+*/
 exports.handler = async (event) => {
     
-    if (!event || !event["email"] || !event["name"]) {
-        return {
-            statusCode: 400,
-            body: "ERROR: Invalid paremeters sent"
-        }
-    }
-    
-    // params "object" of what it being inserted into tabel
+    // Uncomment below to see what the event looks like
+    // console.log("Event object", event);
+
+    // params "object" of what it being inserted into table
     const params = {
+        // ** TODO: CHANGE TableName TO THE NAME OF THE TABLE YOU WANT **
         TableName : "name-of-table",
-        Item : {
-            "primary-key-of-table" : event["email"],
-            "name" : event["name"]
-        }
+        Item: event
     }
     
+    // Return success object, the body is the actual call to DynamoDB
     return {
         statusCode: 200,
-        body: await client.put(params).promise()
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
+        // Need to stringify since we are using "Lambda Proxy Integration"
+        // Read more: https://stackoverflow.com/a/53955884
+        body: JSON.stringify(await dynamoDBClient.put(params).promise())
     };
 };
